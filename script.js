39ba4af0cae6606717bd78a759e91a709ad69c8d
B. Drawe <brycerdrawe.b@gmail.com>
2020-08-29 21:55:24 -0600

    fixed local storage recall

--- script.js
+++ script.js
@@ -3,8 +3,34 @@ todoItems = [
     "","","","","","","","",
 ];
 //Load previously saved tasks
-localStorage.getItem(JSON.parseInt("todoItems"));
+var loadTasks = function(){
+todoItems = JSON.parse(localStorage.getItem("todoItems"));
+$("#0").val(todoItems[0]);
+$("#1").val(todoItems[1]);
+$("#2").val(todoItems[2]);
+$("#3").val(todoItems[3]);
+$("#4").val(todoItems[4]);
+$("#5").val(todoItems[5]);
+$("#6").val(todoItems[6]);
+$("#7").val(todoItems[7]);
 
+// console.log(todoItems);
+
+// if(!todoItems){
+//     todoItems = ["","","","","","","",""]
+// }
+
+// for(var i = 0; i<todoItems.length; i++){
+   
+//     $(".col-10").val(todoItems);
+// }
+// $.each(todoItems,function(value){
+//    $(".col-10").val(todoItems[value]);
+// });
+}
+
+
+    loadTasks();
 //**DONE** Current day is displayed at the top of the calendar 
 var today = $("#currentDay")
     .text(moment().format("dddd, MMM Do"));
@@ -13,20 +39,10 @@ var today = $("#currentDay")
 
 
 //**DONE** When I click the time block I can enter an event
-$(".row").on("click",".col-10", function(){
+var createItem = $(".row").on("click",".col-10", function(){
     var text = $(this)
         .text()
         .trim()
-    //     console.log(text);
-    
-    // // //create textarea  element
-    // var newTask = $("<textarea>")
-    //     .addClass("form-control col-10")
-    //     .val(text);
-    
-    //     $(this).replaceWith(newTask);
-    //     newTask.trigger("focus");
-
 });
 
 
@@ -34,17 +50,7 @@ $(".row").on("click",".col-10", function(){
 $(".saveBtn").on("click", function(){
     var getText = $(this).siblings(".col-10").val();
     var getTextId = $(this).siblings(".col-10").attr("id");
-    console.log(getTextId);
-    //var newTextId = parseInt(getTextId);
-    // console.log(newTextId);
-    // console.log(todoItems);
-    // //create and array of strings
-    //modify the strings
-    todoItems[getTextId]= getText;
-    // console.log(todoItems);
-  
-
     
-
+    todoItems[getTextId]= getText;
     localStorage.setItem('todoItems',JSON.stringify(todoItems))
 });
